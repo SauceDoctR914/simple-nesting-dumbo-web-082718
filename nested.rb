5cf2427bf81b1690b23aae7281a50ad696467422
Gavin Soko <sokolofg@gmail.com>
2018-08-21 21:08:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
      }
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -120,5 +120,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 end
